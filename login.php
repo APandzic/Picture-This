7fb6d4b481895503de5888b0c47bdb7378a0a914
Andreas Pandzic <andreas.pandzic@hotmail.com>
2019-12-11 14:03:38 +0100

    pages for the minimum requirement of features

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 <article>
-    <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+    <p>This is the login page.</p>
 </article>
 <?php require __DIR__ . '/views/footer.php'; ?>
---
+++ views/navigation.php
@@ -0,0 +1,38 @@
+<nav>
+    <a href="#"><?php echo $config['title']; ?></a>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/login.php">login</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/editavatar.php">editavatar</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/editlike.php">editlike</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/editpost.php">editpost</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/editsettings.php">editsettings</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/newpost.php">newpost</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="/reg.php">registration</a>
+        </li>
+
+    </ul>
+</nav>
