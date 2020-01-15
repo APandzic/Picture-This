03bab756d75fc558216d6b7fc8fb115fa317cbd5
Andreas Pandzic <andreas.pandzic@hotmail.com>
2020-01-15 14:29:25 +0100

    bug fix

--- app/users/edit-avatar.php
+++ app/users/edit-avatar.php
@@ -18,12 +18,12 @@ if (isset($_FILES['avatar'])) {
     // validates the file and change file name.
     if (count($_FILES) > 1) {
         $_SESSION['message'] = 'You are not allowed to upload multiple files.';
-        redirect('/editavatar.php');
+        redirect('/edit-avatar.php');
     }
 
     if ($avatar['size'] >= 3145728) {
         $_SESSION['message'] = 'The uploaded file ' . $avatar['name'] . ' exceeded the filesize limit.';
-        redirect('/editavatar.php');
+        redirect('/edit-avatar.php');
     }
 
     if ($avatar['type'] === 'image/jpeg' || $avatar['type'] === 'image/jpg' || $avatar['type'] === 'image/png') {
@@ -31,7 +31,7 @@ if (isset($_FILES['avatar'])) {
         move_uploaded_file($avatar['tmp_name'], $destination);
     } else {
         $_SESSION['message'] = 'The ' . $avatar['name'] . ' image file type is not allowed.';
-        redirect('/editavatar.php');
+        redirect('/edit-avatar.php');
     }
 
     // update avatar string in DB.
